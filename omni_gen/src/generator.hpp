06ca34f75241e43df184b7989a0ae564323e9c9c
Nilanjan Goswami <nilanjan.goswami@gmail.com>
2019-03-21 23:12:54 -0700

    omni_gen Put() function input signature defined

--- omni_gen/src/generator.hpp
+++ omni_gen/src/generator.hpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <list>
 
 namespace ns_omni_gen {
 class Genarator {
@@ -7,6 +9,12 @@ private:
 public:
 	Genarator();
 	virtual ~Genarator() {}
+
+	void Put(std::list<std::string>& fn_info) {
+		//
+		// return_type->func_name->arg1_type->arg1_val->arg2_type->arg2_val...
+		//
+	}
 
 	bool Test() {}
 };
